9abbc814a9d6cbc6c66ec1655d8f9481b6581383
Caio Magri <kaioeduardofm@gmail.com>
2022-09-28 22:46:39 -0300

    Fixing problem with fiscal positon on websitesale to get by region and partner type

--- l10n_br_account/models/fiscal_position.py
+++ l10n_br_account/models/fiscal_position.py
@@ -85,19 +85,90 @@ class AccountFiscalPosition(models.Model):
     )
 
     @api.model
-    def _get_fpos_by_region(
-        self, country_id=False, state_id=False, zipcode=False, vat_required=False
-    ):
-        fpos = super(AccountFiscalPosition, self)._get_fpos_by_region(
-            country_id=country_id, state_id=state_id, zipcode=zipcode,
-            vat_required=vat_required)
-        # type_inv = self.env.context.get('type', False)
-        # supplier = self.env.context.get('search_default_supplier', False)
-        # customer = self.env.context.get('search_default_customer', False)
-        # if type_inv == 'in_invoice' or supplier:
-        #     type_inv = 'entrada'
-        # elif type_inv == 'out_invoice' or customer:
-        #     type_inv = 'saida'
-        # fpos = self.search([('auto_apply', '=', True),
-        #                     ('fiscal_type', '=', type_inv)], limit=1)
+    def _get_fpos_by_region(self,country_id=False, state_id=False, zipcode=False, vat_required=False, partner=False):
+        if not partner:
+            return super(AccountFiscalPosition, self)._get_fpos_by_region(
+                country_id=country_id, state_id=state_id, zipcode=zipcode,vat_required=vat_required
+            )
+
+        if not country_id:
+            return False
+
+        ind_final = '1' if partner.company_type == 'person' else '0'
+
+        base_domain = [
+            ('auto_apply', '=', True),
+            ('vat_required', '=', vat_required),
+            ('company_id', 'in', [self.env.company.id, False]),
+            ('ind_final', '=', ind_final)
+        ]
+        null_state_dom = state_domain = [('state_ids', '=', False)]
+        null_zip_dom = zip_domain = [('zip_from', '=', False), ('zip_to', '=', False)]
+        null_country_dom = [('country_id', '=', False), ('country_group_id', '=', False)]
+
+        if zipcode:
+            zip_domain = [('zip_from', '<=', zipcode), ('zip_to', '>=', zipcode)]
+
+        if state_id:
+            state_domain = [('state_ids', '=', state_id)]
+
+        domain_country = base_domain + [('country_id', '=', country_id)]
+        domain_group = base_domain + [('country_group_id.country_ids', '=', country_id)]
+
+        # Build domain to search records with exact matching criteria
+        fpos = self.search(domain_country + state_domain + zip_domain, limit=1)
+        # return records that fit the most the criteria, and fallback on less specific fiscal positions if any can be found
+        if not fpos and state_id:
+            fpos = self.search(domain_country + null_state_dom + zip_domain, limit=1)
+        if not fpos and zipcode:
+            fpos = self.search(domain_country + state_domain + null_zip_dom, limit=1)
+        if not fpos and state_id and zipcode:
+            fpos = self.search(domain_country + null_state_dom + null_zip_dom, limit=1)
+
+        # fallback: country group with no state/zip range
+        if not fpos:
+            fpos = self.search(domain_group + null_state_dom + null_zip_dom, limit=1)
+
+        if not fpos:
+            # Fallback on catchall (no country, no group)
+            fpos = self.search(base_domain + null_country_dom, limit=1)
         return fpos
+
+    @api.model
+    def get_fiscal_position(self, partner_id, delivery_id=None):
+        """
+        :return: fiscal position found (recordset)
+        :rtype: :class:`account.fiscal.position`
+        """
+        if not partner_id:
+            return self.env['account.fiscal.position']
+
+        # This can be easily overridden to apply more complex fiscal rules
+        PartnerObj = self.env['res.partner']
+        partner = PartnerObj.browse(partner_id)
+        delivery = PartnerObj.browse(delivery_id)
+
+        company = self.env.company
+        eu_country_codes = set(self.env.ref('base.europe').country_ids.mapped('code'))
+        intra_eu = vat_exclusion = False
+        if company.vat and partner.vat:
+            intra_eu = company.vat[:2] in eu_country_codes and partner.vat[:2] in eu_country_codes
+            vat_exclusion = company.vat[:2] == partner.vat[:2]
+
+        # If company and partner have the same vat prefix (and are both within the EU), use invoicing
+        if not delivery or (intra_eu and vat_exclusion):
+            delivery = partner
+
+        # partner manually set fiscal position always win
+        if delivery.property_account_position_id or partner.property_account_position_id:
+            return delivery.property_account_position_id or partner.property_account_position_id
+
+        # First search only matching VAT positions
+        vat_required = bool(partner.vat)
+        fp = self._get_fpos_by_region(delivery.country_id.id, delivery.state_id.id, delivery.zip, vat_required, partner)
+
+        # Then if VAT required found no match, try positions that do not require it
+        if not fp and vat_required:
+            fp = self._get_fpos_by_region(delivery.country_id.id, delivery.state_id.id, delivery.zip, False, partner)
+
+        return fp or self.env['account.fiscal.position']
